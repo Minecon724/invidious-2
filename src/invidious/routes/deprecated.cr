7c07621235249a25f44e3fbc47099b539331a5df
Minecon724 <39800107+Minecon724@users.noreply.github.com>
2025-06-07 12:56:16 +0200

    MAybe this

--- src/invidious/routes/deprecated.cr
+++ src/invidious/routes/deprecated.cr
@@ -3,7 +3,7 @@
 module Invidious::Routes::Deprecated
   def self.opportunistic_notice(env)
     if env.get? "user"
-      templated "deprecated"
+      rendered "deprecated"
     end
 
     return Invidious::Routes::Misc.cross_instance_redirect(env)
@@ -11,7 +11,7 @@ module Invidious::Routes::Deprecated
   
 
   def self.deprecated_notice(env)
-    templated "deprecated"
+    rendered "deprecated"
   end
 
   def self.deprecated_notice_raw(env)
